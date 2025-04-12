e08e93022a998bd3fed62dfb84d437f68cdda6d9
EugeneSt <eugene.stepchenko@gmail.com>
2025-04-12 08:52:31 +0300

    Initial Commit

---
+++ SwiftUICurrency/SwiftUICurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICurrencyApp.swift
+//  SwiftUICurrency
+//
+//  Created by Eugene St on 12.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
